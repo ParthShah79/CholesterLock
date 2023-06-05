246d9ca8156661acb1302fe42862b1027d359777
ParthShah79 <parths7979@gmail.com>
2023-06-05 13:15:12 -0600

    Vertical Progress Bar

--- app.js
+++
@@ -1,8 +0,0 @@
-const filled = document.guerySelector('.filled');
-
-function undate () {
-    filled.style.width = '${((window.scrollY) / (document.body.scrollHeight - window.innerHeight)*100)}% '
-   requestAnimationFrame(update);
-}
-
-update();
